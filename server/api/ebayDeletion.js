3e481387b38131bdf858c05b10e5fe57de29d38f
Johnny Gulo <johngulo30@gmail.com>
2025-04-07 16:15:14 -0500

    Force 200 status and text/plain for eBay

--- server/api/ebayDeletion.js
+++ server/api/ebayDeletion.js
@@ -5,8 +5,8 @@ const VERIFICATION_TOKEN = 'zonecard-token-verify-2025-secure-production-abc123'
 
 router.post('/ebay/deletion', (req, res) => {
   console.log('🔔 Received eBay Deletion Notification');
-  res.set('Content-Type', 'text/plain'); // 👈 Force correct response type
-  res.send(VERIFICATION_TOKEN);
+  res.set('Content-Type', 'text/plain');
+  res.status(200).send('zonecard-token-verify-2025-secure-production-abc123');
 });
 
 export default router;
